dd993dd9f72a116c820952557076c5ec446d10c6
Me-Nicolas <Nicolas@nicolas-macbook-pro.local>
2016-09-12 16:17:41 +0200

    Add MapViewController + Xib

---
+++ GitTutorial/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  GitTutorial
+//
+//  Created by Nicolas TRUTET on 12/09/2016.
+//  Copyright © 2016 Nicolas TRUTET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ GitTutorial/MapViewController.m
@@ -0,0 +1,29 @@
+//
+//  MapViewController.m
+//  GitTutorial
+//
+//  Created by Nicolas TRUTET on 12/09/2016.
+//  Copyright © 2016 Nicolas TRUTET. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
