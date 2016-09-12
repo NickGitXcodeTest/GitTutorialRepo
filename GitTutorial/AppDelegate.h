cb188febb5719c91023dace95c7f26776a918fa6
Me-Nicolas <Nicolas@nicolas-macbook-pro.local>
2016-09-12 15:52:22 +0200

    Initial Commit

---
+++ GitTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTutorial
+//
+//  Created by Nicolas TRUTET on 12/09/2016.
+//  Copyright © 2016 Nicolas TRUTET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
